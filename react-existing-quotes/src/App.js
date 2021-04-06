29a8930b44321dbd068cd2a1741a77f2a6dc75c9
Gert Braspenning <gert.braspenning@cronos.be>
2021-04-06 22:46:54 +0200

    get data from outer frame

--- react-existing-quotes/src/App.js
+++ react-existing-quotes/src/App.js
@@ -4,9 +4,41 @@ import './App.css';
 import { Component } from 'react'
 import MaterialTable from 'material-table'
 
-
+// Get data from CPQ using iframe.contentWindow.postMessage(ko.toJSON(cpq.models.cartList.mainGrid), "https://brspnnggrt.github.io/")
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      // default columns & row data
+      columns: [
+        { title: 'Adı', field: 'name' },
+        { title: 'Soyadı', field: 'surname' },
+        { title: 'Doğum Yılı', field: 'birthYear', type: 'numeric' },
+        { title: 'Doğum Yeri', field: 'birthCity', lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' } }
+      ],
+      data: [{ name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 }],
+    };
+    this.onMessageReceived = event => {
+      if (!event.data.source) {
+        console.log(event.data);
+        this.setState({
+          loading: false,
+          columns: event.data.columns.map(col => { return { title: col.title, field: col.name}; }),
+          data: event.data.rows.map(row => { 
+            let keys = row.cells.slice(1).map(t => t.columnName);
+            let values = row.cells.slice(1).map(t => t.value);
+            let data = Object.fromEntries(keys.map((_, i) => [keys[i], values[i]]));
+            return data;
+          })
+        });
+      }
+    };
+    window.addEventListener("message", this.onMessageReceived, false);
+  }
+
   render() {
     return (
       <div className="App">
@@ -14,13 +46,8 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
         </header>
         <MaterialTable
-            columns={[
-              { title: 'Adı', field: 'name' },
-              { title: 'Soyadı', field: 'surname' },
-              { title: 'Doğum Yılı', field: 'birthYear', type: 'numeric' },
-              { title: 'Doğum Yeri', field: 'birthCity', lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' } }
-            ]}
-            data={[{ name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 }]}
+            columns={this.state.columns}
+            data={this.state.data}
             title="Demo Title"
             actions={[
               {
