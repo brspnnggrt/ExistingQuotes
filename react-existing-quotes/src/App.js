65c6831a2053084c85467c195549c1902ad4e10c
Gert <braspenninggert@gmail.com>
2021-05-19 23:26:14 +0200

    Update build

--- react-existing-quotes/src/App.js
+++ react-existing-quotes/src/App.js
@@ -61,9 +61,9 @@ class App extends Component {
       // transform functions
       let mapColumns = c => c.map(col => { return { title: col.vrLabel, field: col.vrColumnName }; });
       let newColumns = visibleColumns.length ? mapColumns(visibleColumns): this.state.columns;
-      let mapRowsToData = r => r.map(rrow => { 
-        let filteredEntries = Object.entries(rrow.Data).filter((k, v) => newColumns.includes(k));
-        let mappedEntries = filteredEntries.map((k,v) => (k, v.Value));
+      let mapRowsToData = r => r.map(row => { 
+        let filteredEntries = Object.entries(row.Data).filter(([k,v]) => newColumns.map(nc => nc.vrColumnName).includes(k));
+        let mappedEntries = filteredEntries.map(([k,v]) => (k, v.Value));
         return Object.fromEntries(mappedEntries);
       });
       let newData = rows.length ? mapRowsToData(rows) : this.state.data;
